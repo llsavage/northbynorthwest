a232139f93ad3ea104230755bb03f792d73aee5f
llsavage <lsavage@cca.edu>
2019-01-11 16:18:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function(){
+	var sweetTooth = new Audio();
+	sweetTooth.src = "Audio/SomethingForYourSweetTooth.mp3";
+function PlaySound() {
+    sweetTooth.play();
+}
+	
+	});
